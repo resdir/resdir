59b4f5cdd84e800ae5f2ca6e3260384ed8664e93
Manuel Vila <mvila@3base.com>
2017-08-08 18:38:59 +0900

    @resdir/version: Add normalize(), serialize() and deserialize() static methods

--- packages/version/src/index.js
+++ packages/version/src/index.js
@@ -10,6 +10,13 @@ export class Version {
     this.value = value;
   }
 
+  static normalize(version) {
+    if (typeof version === 'string') {
+      version = new this(version);
+    }
+    return version;
+  }
+
   bump(part = 'patch') {
     this.value = semver.inc(this.value, part);
     return this;
@@ -22,6 +29,62 @@ export class Version {
   toString() {
     return this.value;
   }
+
+  static serialize(version) {
+    version = this.normalize(version);
+
+    const value = version.value;
+    const major = serializeNumber(semver.major(value));
+    const minor = serializeNumber(semver.minor(value));
+    const patch = serializeNumber(semver.patch(value));
+    const prerelease = semver.prerelease(value);
+
+    let result = major + '.' + minor + '.' + patch;
+    if (prerelease) {
+      result += '-' + prerelease.join('.');
+    }
+    return result;
+  }
+
+  static deserialize(version) {
+    if (typeof version !== 'string') {
+      throw new Error('\'version\' argument must be a string');
+    }
+
+    let [numbers, prerelease] = version.split('-');
+
+    numbers = numbers.split('.');
+    if (numbers.length !== 3) {
+      throw new Error(`Invalid serialized version: ${version}`);
+    }
+    const major = deserializeNumber(numbers[0]);
+    const minor = deserializeNumber(numbers[1]);
+    const patch = deserializeNumber(numbers[2]);
+
+    let result = String(major) + '.' + String(minor) + '.' + String(patch);
+
+    if (prerelease) {
+      result += '-' + prerelease;
+    }
+
+    return result;
+  }
+}
+
+function serializeNumber(number) {
+  if (number > 99999) {
+    throw new Error('Can\'t serialize a number greater than 99999');
+  }
+  number = '0000' + String(number);
+  number = number.slice(-5);
+  return number;
+}
+
+function deserializeNumber(str) {
+  if (str.length !== 5) {
+    throw new Error(`Invalid serialized version number: ${str}`);
+  }
+  return Number(str);
 }
 
 export default Version;
--- packages/version/tests/src/index.test.js
+++ packages/version/tests/src/index.test.js
@@ -13,19 +13,44 @@ describe('Version', () => {
     expect(() => new Version('^1.0.0')).toThrow();
   });
 
+  test('normalization', () => {
+    expect(Version.normalize('1.0.0')).toBeInstanceOf(Version);
+    expect(Version.normalize('1.0.0').toString()).toBe('1.0.0');
+    const version = new Version('1.0.0');
+    expect(Version.normalize(version)).toBe(version);
+  });
+
   test('bumping', () => {
     expect(new Version('1.0.0').bump().toString()).toBe('1.0.1');
     expect(new Version('1.0.0').bump('patch').toString()).toBe('1.0.1');
-    expect(new Version('1.0.0').bump('minor').toString()).toBe('1.1.0');
-    expect(new Version('1.0.0').bump('major').toString()).toBe('2.0.0');
+    expect(new Version('1.0.5').bump('minor').toString()).toBe('1.1.0');
+    expect(new Version('1.5.0').bump('major').toString()).toBe('2.0.0');
   });
 
-  test('serialization', () => {
+  test('stringification', () => {
     expect(new Version('1.2.3').toJSON()).toBe('1.2.3');
     expect(new Version('1.2.3').toString()).toBe('1.2.3');
   });
 
   test('conversion', () => {
     expect('v' + new Version('1.2.3')).toBe('v1.2.3');
+  });
+
+  test('serialization', () => {
+    expect(Version.serialize('1.2.3')).toBe('00001.00002.00003');
+    expect(Version.serialize('0.0.123')).toBe('00000.00000.00123');
+    expect(Version.serialize('0.1.0-alpha.1')).toBe('00000.00001.00000-alpha.1');
+    expect(() => Version.serialize('0.0.99999')).not.toThrow();
+    expect(Version.serialize('0.0.99999')).toBe('00000.00000.99999');
+    expect(() => Version.serialize('0.0.100000')).toThrow();
+  });
+
+  test('deserialization', () => {
+    expect(Version.deserialize('00001.00002.00003')).toBe('1.2.3');
+    expect(Version.deserialize('00000.00000.00123')).toBe('0.0.123');
+    expect(Version.deserialize('00000.00001.00000-alpha.1')).toBe('0.1.0-alpha.1');
+    expect(() => Version.deserialize('00000.00000.99999')).not.toThrow();
+    expect(Version.deserialize('00000.00000.99999')).toBe('0.0.99999');
+    expect(() => Version.deserialize('00000.00000.100000')).toThrow();
   });
 });
