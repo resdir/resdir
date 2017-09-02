465506d14139c15ef25d56371ceac60e7bd415ac
Manuel Vila <mvila@3base.com>
2017-09-02 09:27:07 +0900

    @resdir/information: Edit some information

--- packages/information/src/index.js
+++ packages/information/src/index.js
@@ -1,5 +1,7 @@
 export const SERVICE_NAME = 'Resdir';
-export const WEBSITE_URL = 'https://resdir.com';
-export const GENERAL_EMAIL_ADDRESS = 'hello@resdir.com';
+export const SERVICE_URL = 'https://resdir.com';
+export const SERVICE_EMAIL_ADDRESS = 'hello@resdir.com';
+export const SUPPORT_NAME = 'Resdir support';
 export const SUPPORT_EMAIL_ADDRESS = 'support@resdir.com';
+export const NO_REPLY_NAME = 'Resdir';
 export const NO_REPLY_EMAIL_ADDRESS = 'no-reply@resdir.com';
