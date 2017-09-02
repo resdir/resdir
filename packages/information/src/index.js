05cc6384b931dfb12129e7f0b6369a0822701c96
Manuel Vila <mvila@3base.com>
2017-09-02 09:02:12 +0900

    Add @resdir/information

---
+++ packages/information/src/index.js
@@ -0,0 +1,5 @@
+export const SERVICE_NAME = 'Resdir';
+export const WEBSITE_URL = 'https://resdir.com';
+export const GENERAL_EMAIL_ADDRESS = 'hello@resdir.com';
+export const SUPPORT_EMAIL_ADDRESS = 'support@resdir.com';
+export const NO_REPLY_EMAIL_ADDRESS = 'no-reply@resdir.com';
