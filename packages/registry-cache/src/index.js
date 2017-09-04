a385cff627fcf1dd94ac93c3f760f4ca299a6f1a
Manuel Vila <mvila@3base.com>
2017-09-04 09:32:30 +0900

    @resdir/registry-client: Implement user sign up, sign in, etc.

--- packages/registry-cache/src/index.js
+++ packages/registry-cache/src/index.js
@@ -11,7 +11,8 @@ import VersionRange from '@resdir/version-range';
 
 const debug = require('debug')('resdir:registry:cache');
 
-const CACHE_DIRECTORY = join(homedir(), '.resdir', 'caches', 'registry-cache');
+const RESDIR_DIRECTORY = join(homedir(), '.resdir');
+const CACHE_DIRECTORY = join(RESDIR_DIRECTORY, 'caches', 'registry-cache');
 const RESOURCE_CACHE_DIRECTORY = join(CACHE_DIRECTORY, 'resources');
 const RESOURCE_REQUESTS_DIRECTORY_NAME = 'requests';
 const RESOURCE_VERSIONS_DIRECTORY_NAME = 'versions';
@@ -20,23 +21,41 @@ const RESOURCE_FILE_NAME = '@resource.json';
 /*
 Cache is stored on disk as follow:
 
-  registry-cache
-    resources
-      resdir
-        example
-          requests
-            _.json => {"version":"0.2.0","expiresOn":"..."}
-            ^0.1.0.json => {"version":"0.1.2","expiresOn":"..."}
-          versions
-            0.1.2
-              @resource.json
-              dist
-                index.js
+.resdir
+  caches
+    registry-cache
+      resources
+        resdir
+          example
+            requests
+              _.json => {"version":"0.2.0","expiresOn":"..."}
+              ^0.1.0.json => {"version":"0.1.2","expiresOn":"..."}
+            versions
+              0.1.2
+                @resource.json
+                dist
+                  index.js
 */
 
 export class RegistryCache {
   constructor(client) {
     this.client = client;
+  }
+
+  async signUp(...args) {
+    return await this.client.signUp(...args);
+  }
+
+  async signIn(...args) {
+    return await this.client.signIn(...args);
+  }
+
+  async signOut(...args) {
+    return await this.client.signOut(...args);
+  }
+
+  async createUserNamespace(...args) {
+    return await this.client.createUserNamespace(...args);
   }
 
   async fetch(specifier) {
@@ -217,11 +236,6 @@ export class RegistryCache {
   }
 
   async publish(definition, directory) {
-    await this._publish(definition, directory);
-    debug('publish(%o, %o)', definition, directory);
-  }
-
-  async _publish(definition, directory) {
     await this.client.publish(definition, directory);
     await this._invalidateCache(definition['@name'], definition['@version']);
   }
--- packages/registry-client/src/index.js
+++ packages/registry-client/src/index.js
@@ -1,15 +1,16 @@
-import {resolve} from 'path';
+import {join, resolve, dirname} from 'path';
 import {existsSync} from 'fs';
-// import {homedir} from 'os';
-// import {outputFileSync, emptyDir} from 'fs-extra';
+import {homedir} from 'os';
+import {upperFirst} from 'lodash';
+import {ensureDirSync} from 'fs-extra';
 import S3 from 'aws-sdk/clients/s3';
 import hasha from 'hasha';
 import s3urls from '@mapbox/s3urls';
 import tempy from 'tempy';
 import isDirectory from 'is-directory';
 import readDirectory from 'recursive-readdir';
-import {formatPath, formatCode} from '@resdir/console';
-// import {load, save} from '@resdir/file-manager';
+import {formatString, formatPath, formatCode, prompt, task} from '@resdir/console';
+import {load, save} from '@resdir/file-manager';
 import {validateResourceSpecifier} from '@resdir/resource-specifier';
 import generateSecret from '@resdir/secret-generator';
 import {getJSON, postJSON, fetch} from '@resdir/http-client';
@@ -17,13 +18,109 @@ import {zip, unzip} from '@resdir/archive-manager';
 
 const debug = require('debug')('resdir:registry:client');
 
+const RESDIR_DIRECTORY = join(homedir(), '.resdir');
+const USER_FILE = join(RESDIR_DIRECTORY, 'user.json');
+const CACHE_FILE = join(RESDIR_DIRECTORY, 'caches', 'registry-client', 'data.json');
+
 const RESDIR_REGISTRY_LOCAL_SERVER_URL = 'http://localhost:3000/registry';
 
 export class RegistryClient {
-  constructor({awsRegion, awsS3BucketName, awsS3ResourceUploadsPrefix}) {
+  constructor({awsRegion, awsS3BucketName, awsS3ResourceUploadsPrefix, clientId}) {
     this.awsRegion = awsRegion;
     this.awsS3BucketName = awsS3BucketName;
     this.awsS3ResourceUploadsPrefix = awsS3ResourceUploadsPrefix;
+    this.clientId = clientId;
+  }
+
+  async signUp(email) {
+    return await this._signUpOrSignIn('SIGN_UP', email);
+  }
+
+  async signIn(email) {
+    return await this._signUpOrSignIn('SIGN_IN', email);
+  }
+
+  async _signUpOrSignIn(action, email) {
+    if (this._hasSignedInUser()) {
+      throw new Error('A user is already signed in');
+    }
+
+    while (!email) {
+      email = await prompt('Enter your email address:');
+    }
+
+    const messagePart = action === 'SIGN_UP' ? 'sign up' : 'sign in';
+    const urlPart = action === 'SIGN_UP' ? 'sign-up' : 'sign-in';
+
+    await task(
+      async () => {
+        const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/user/start-${urlPart}`;
+        await postJSON(url, {email, clientId: this.clientId});
+      },
+      {
+        intro: `Sending verification token to ${formatString(email)}...`,
+        outro: `Verification token sent to ${formatString(email)}`
+      }
+    );
+
+    let completed;
+    while (!completed) {
+      const verificationToken = await prompt(
+        'Check your mailbox and copy/paste the received token:'
+      );
+      if (!verificationToken) {
+        continue;
+      }
+      await task(
+        async () => {
+          const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/user/complete-${urlPart}`;
+          const {userId, refreshToken} = await postJSON(url, {verificationToken});
+          this._saveUserId(userId);
+          this._saveRefreshToken(refreshToken);
+          completed = true;
+        },
+        {intro: `Completing ${messagePart}...`, outro: `${upperFirst(messagePart)} completed`}
+      );
+    }
+
+    if (action === 'SIGN_UP') {
+      await this.createUserNamespace();
+    }
+  }
+
+  async signOut() {
+    this._ensureSignedInUser();
+
+    await task(
+      async () => {
+        const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/user/sign-out`;
+        const refreshToken = this._loadRefreshToken();
+        await postJSON(url, {refreshToken});
+        this._saveUserId(undefined);
+        this._saveRefreshToken(undefined);
+        this._saveAccessToken(undefined);
+      },
+      {intro: `Signing out...`, outro: `Signed out`}
+    );
+  }
+
+  async createUserNamespace(namespace) {
+    const userId = this._ensureSignedInUser();
+
+    while (!namespace) {
+      namespace = await prompt('Choose a name for your personal namespace:');
+    }
+
+    await task(
+      async () => {
+        const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/users/${userId}/namespace`;
+        await this._userRequest(authorization => postJSON(url, {namespace}, {authorization}));
+      },
+      {
+        intro: `Creating namespace ${formatString(namespace)}...`,
+        outro: `Namespace ${formatString(namespace)} created`
+      }
+    );
   }
 
   async fetch(specifier, options) {
@@ -39,7 +136,10 @@ export class RegistryClient {
     if (cachedVersion) {
       url += `?cachedVersion=${cachedVersion}`;
     }
-    const {unchanged, definition, filesURL} = await getJSON(url);
+
+    const {unchanged, definition, filesURL} = await this._userRequest(authorization =>
+      getJSON(url, {authorization})
+    );
 
     if (unchanged) {
       return {unchanged};
@@ -60,6 +160,8 @@ export class RegistryClient {
   }
 
   async _publish(definition, directory) {
+    this._ensureSignedInUser();
+
     const name = definition['@name'];
     if (!name) {
       throw new Error(`Can't publish a resource without a ${formatCode('@name')} property`);
@@ -89,8 +191,9 @@ export class RegistryClient {
     const temporaryFilesURL = s3urls.toUrl(this.awsS3BucketName, key)['bucket-in-host'];
 
     const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/resources`;
-    const body = {definition, temporaryFilesURL};
-    await postJSON(url, {body});
+    await this._userRequest(authorization =>
+      postJSON(url, {definition, temporaryFilesURL}, {authorization})
+    );
   }
 
   async _getFiles(definition, directory) {
@@ -115,6 +218,141 @@ export class RegistryClient {
     }
 
     return files;
+  }
+
+  // === User ===
+
+  _ensureSignedInUser({throwIfNoSignedInUser = true} = {}) {
+    const userId = this._loadUserId();
+    if (!userId) {
+      if (throwIfNoSignedInUser) {
+        throw new Error('No user is signed in');
+      }
+      return undefined;
+    }
+    return userId;
+  }
+
+  _hasSignedInUser() {
+    return Boolean(this._loadUserId());
+  }
+
+  _loadUserId() {
+    if (!this._userId) {
+      this._userId = this._loadUserData().id;
+    }
+    return this._userId;
+  }
+
+  _saveUserId(id) {
+    this._userId = id;
+    this._updateUserData({id});
+  }
+
+  _loadUserData() {
+    return load(USER_FILE, {throwIfNotFound: false}) || {};
+  }
+
+  _saveUserData(data) {
+    ensureDirSync(dirname(USER_FILE));
+    save(USER_FILE, data);
+  }
+
+  _updateUserData(data) {
+    const userData = this._loadUserData();
+    Object.assign(userData, data);
+    this._saveUserData(userData);
+  }
+
+  // === Authentication ===
+
+  async _userRequest(fn) {
+    let accessToken = await this._loadAccessToken();
+    const request = async () => {
+      const authorization = accessToken && `Bearer ${accessToken}`;
+      return await fn(authorization);
+    };
+    try {
+      return await request();
+    } catch (err) {
+      if (err.httpStatus === 401) {
+        accessToken = await this._refreshAccessToken();
+        return await request();
+      }
+      throw err;
+    }
+  }
+
+  _loadRefreshToken() {
+    if (!this._refreshToken) {
+      this._refreshToken = this._loadUserData().refreshToken;
+    }
+    return this._refreshToken;
+  }
+
+  _saveRefreshToken(refreshToken) {
+    this._refreshToken = refreshToken;
+    this._updateUserData({refreshToken});
+  }
+
+  async _loadAccessToken() {
+    if (!this._accessToken) {
+      const cache = this._loadCache();
+      this._accessToken = cache.accessToken;
+      if (this._accessToken) {
+        this._accessToken.expiresOn = new Date(this._accessToken.expiresOn);
+      }
+    }
+    if (!this._accessToken) {
+      await this._refreshAccessToken();
+    }
+    if (!this._accessToken) {
+      return undefined;
+    }
+    if (Date.now() > this._accessToken.expiresOn - 3 * 60 * 1000) {
+      // Refresh access token 3 minutes before expiration
+      await this._refreshAccessToken();
+    }
+    return this._accessToken.value;
+  }
+
+  async _refreshAccessToken() {
+    debug('_refreshAccessToken()');
+    let accessToken;
+    try {
+      const refreshToken = this._loadRefreshToken();
+      if (!refreshToken) {
+        return;
+      }
+      const url = `${RESDIR_REGISTRY_LOCAL_SERVER_URL}/user/access-tokens`;
+      const {accessToken: {value, expiresIn}} = await postJSON(url, {refreshToken});
+      accessToken = {value, expiresOn: new Date(Date.now() + expiresIn)};
+      return value;
+    } finally {
+      this._saveAccessToken(accessToken);
+    }
+  }
+
+  _saveAccessToken(accessToken) {
+    this._accessToken = accessToken;
+    this._updateCache({accessToken});
+  }
+
+  // === Cache ===
+
+  _loadCache() {
+    return load(CACHE_FILE, {throwIfNotFound: false}) || {};
+  }
+
+  _saveCache(data) {
+    ensureDirSync(dirname(CACHE_FILE));
+    save(CACHE_FILE, data);
+  }
+
+  _updateCache(data) {
+    const cache = this._loadCache();
+    Object.assign(cache, data);
+    this._saveCache(cache);
   }
 }
 
