784fd9146e3741390831f5fdf6490a3f6ca5faa1
Manuel Vila <mvila@3base.com>
2017-11-13 11:31:33 +0900

    @resdir/npm-link: Add package automatic search feature

--- packages/npm-link/src/index.js
+++ packages/npm-link/src/index.js
@@ -1,45 +1,35 @@
-import {resolve, join, relative} from 'path';
-import {existsSync} from 'fs';
-import {removeSync, ensureSymlink} from 'fs-extra';
+import {resolve, join, relative, dirname} from 'path';
+import {existsSync, readdirSync} from 'fs';
+import {removeSync, ensureSymlinkSync} from 'fs-extra';
 import {load} from '@resdir/file-manager';
+import isDirectory from 'is-directory';
+import minimist from 'minimist';
 
 export function run() {
-  const currentDirectory = process.cwd();
+  const argv = minimist(process.argv, {
+    string: ['directory'],
+    alias: {
+      directory: ['dir', 'd']
+    }
+  });
 
-  let packageDirectory = process.argv[2];
+  let currentDirectory = process.cwd();
+  if (argv.directory) {
+    currentDirectory = resolve(currentDirectory, argv.directory);
+  }
+
+  const packageName = process.argv[2];
+
+  if (!packageName) {
+    throw new Error('\'packageName\' is missing');
+  }
+
+  const config = loadConfig(currentDirectory);
+
+  const packageDirectory = findPackage(config.directories, packageName);
 
   if (!packageDirectory) {
-    throw new Error('\'path\' argument is missing');
-  }
-
-  packageDirectory = resolve(currentDirectory, packageDirectory);
-
-  let name;
-
-  const packageFile = join(packageDirectory, 'package.json');
-  const pkg = load(packageFile, {throwIfNotFound: false});
-  if (pkg) {
-    name = pkg.name;
-  }
-
-  if (!name) {
-    const resourceFile = join(packageDirectory, '@resource.json');
-    const resourceDefinition = load(resourceFile, {throwIfNotFound: false});
-    if (resourceDefinition) {
-      name = resourceDefinition.name;
-    }
-  }
-
-  if (!name) {
-    const resourceFile = join(packageDirectory, '@resource.json5');
-    const resourceDefinition = load(resourceFile, {throwIfNotFound: false});
-    if (resourceDefinition) {
-      name = resourceDefinition.name;
-    }
-  }
-
-  if (!name) {
-    throw new Error('Can\'t determine package name');
+    throw new Error(`Package '${packageName}' not found`);
   }
 
   const modulesDirectory = join(currentDirectory, 'node_modules');
@@ -48,11 +38,49 @@ export function run() {
     throw new Error('\'node_modules\' directory not found in the current directory');
   }
 
-  const linkFile = join(modulesDirectory, name);
+  const linkFile = join(modulesDirectory, packageName);
 
   removeSync(linkFile);
 
-  ensureSymlink(relative(currentDirectory, packageDirectory), linkFile);
+  ensureSymlinkSync(relative(dirname(linkFile), packageDirectory), linkFile);
 
-  console.log(`'${name}' linked`);
+  console.log(`'${packageName}' linked`);
+}
+
+function findPackage(directories, name) {
+  for (const directory of directories) {
+    const entries = readdirSync(directory);
+    for (let entry of entries) {
+      entry = join(directory, entry);
+      if (!isDirectory.sync(entry)) {
+        continue;
+      }
+      const packageDirectory = entry;
+      const packageFile = join(packageDirectory, 'package.json');
+      const pkg = load(packageFile, {throwIfNotFound: false});
+      if (!pkg) {
+        continue;
+      }
+      if (pkg.name === name) {
+        return packageDirectory;
+      }
+    }
+  }
+}
+
+function loadConfig(directory) {
+  const file = join(directory, '.npm-link.json');
+  if (existsSync(file)) {
+    const config = load(file);
+    const configDirectory = dirname(file);
+    config.directories = config.directories.map(directory => resolve(configDirectory, directory));
+    return config;
+  }
+
+  const parentDirectory = join(directory, '..');
+  if (parentDirectory !== directory) {
+    return loadConfig(parentDirectory);
+  }
+
+  throw new Error('Config file not found');
 }
