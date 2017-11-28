3f1b56261e535c1c3bb92750bfccbcefe9b14019
Manuel Vila <mvila@3base.com>
2017-11-28 13:12:57 +0900

    @resdir/resource-specifier: Add some tests

---
+++ packages/resource-specifier/tests/src/index.test.js
@@ -0,0 +1,20 @@
+import {parseResourceSpecifier} from '../..';
+
+describe('@resdir/resource-specifier', () => {
+  test('parseResourceSpecifier()', () => {
+    expect(parseResourceSpecifier('resdir/hello').identifier).toBe('resdir/hello');
+    expect(parseResourceSpecifier('resdir/hello').versionRange.toString()).toBe('');
+
+    expect(parseResourceSpecifier('resdir/hello#^1.0.0').identifier).toBe('resdir/hello');
+    expect(parseResourceSpecifier('resdir/hello#^1.0.0').versionRange.toString()).toBe('^1.0.0');
+
+    expect(parseResourceSpecifier('./example').location).toBe('./example');
+    expect(parseResourceSpecifier('/Users/bob/dev/example').location).toBe(
+      '/Users/bob/dev/example'
+    );
+
+    expect(() => parseResourceSpecifier('resdir')).toThrow();
+    expect(() => parseResourceSpecifier('resdir/-hello-')).toThrow();
+    expect(() => parseResourceSpecifier('resdir/hello@1.2.3.4')).toThrow();
+  });
+});
