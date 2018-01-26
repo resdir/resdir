55b2ca7f54073e968c1382f5b0509a5457b195b9
Manuel Vila <mvila@3base.com>
2018-01-26 20:59:26 +0900

    resdir/resource: Add 'throwIfAlreadyExists' option to publish()

--- resources/resdir/resource/src/index.js
+++ resources/resdir/resource/src/index.js
@@ -1,15 +1,33 @@
-import {prompt, formatCode} from '@resdir/console';
+import {prompt, formatCode, printSuccess} from '@resdir/console';
 import {validateResourceIdentifier} from '@resdir/resource-identifier';
 import {validateVersion} from '@resdir/version';
+import {stringifyResourceSpecifier} from '@resdir/resource-specifier';
 import GitIgnore from '@resdir/gitignore-manager';
 
 const GIT_IGNORE = ['.DS_STORE', '*.log'];
 
 export default base =>
   class ResdirResource extends base {
-    async publish({major, minor, patch, permissionToken}, environment) {
+    async publish({major, minor, patch, throwIfAlreadyExists, permissionToken}, environment) {
       if (!(this.id && this.version)) {
         throw new Error(`Can't publish a resource without ${formatCode('id')} and ${formatCode('version')} properties`);
+      }
+
+      const registryClient = await this.constructor.$getRegistryClient();
+
+      if (!throwIfAlreadyExists) {
+        if (major || minor || patch) {
+          throw new Error(`Can't disable ${formatCode('throwIfAlreadyExists')} with ${formatCode('major')} or ${formatCode('minor')} or ${formatCode('patch')} options`);
+        }
+        const specifier = stringifyResourceSpecifier({
+          identifier: this.id,
+          versionRange: this.version
+        });
+        const {resource} = await registryClient.resources.get({specifier, throwIfNotFound: false});
+        if (resource) {
+          printSuccess('Publishing aborted');
+          return;
+        }
       }
 
       await this['@build']();
@@ -21,9 +39,8 @@ export default base =>
         await this.$getChild('version').bump({major, minor, patch}, environment);
       }
 
-      const registryClient = await this.constructor.$getRegistryClient();
       const file = this.$getResourceFile();
-      await registryClient.resources.publish({file, permissionToken});
+      await registryClient.resources.publish({file, permissionToken}, environment);
 
       await this.$emit('published');
     }
