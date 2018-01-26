948b2a6417ec892250501f6456262350b6d9b324
Manuel Vila <mvila@3base.com>
2018-01-26 13:41:50 +0900

    aws/hosted-resource: Improve remote invocation protocol

--- resources/aws/hosted-resource/example/src/index.js
+++ resources/aws/hosted-resource/example/src/index.js
@@ -1,4 +1,6 @@
-// time curl -v -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"hello","id":1}' https://api.example.resdir.com
+// time curl -v -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"getMethods","id":1}' https://api.example.resdir.com
+
+// time curl -v -X POST -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"invokeMethod", "params": {"name": "hello"}, "id":1}' https://api.example.resdir.com
 
 export default base =>
   class Example extends base {
--- resources/aws/hosted-resource/lambda-handler/src/index.js
+++ resources/aws/hosted-resource/lambda-handler/src/index.js
@@ -4,23 +4,19 @@ import {
   buildJSONRPCError,
   createJSONRPCError
 } from '@resdir/json-rpc';
+import {createClientError, isClientError} from '@resdir/error';
+
+let definition;
 
 const Base = (() => {
-  const definition = require('./definition.json');
-
-  class Base {
-    __getMethods__() {
-      return definition.methods;
-    }
-  }
-
+  definition = require('./definition.json');
+  class Base {}
   for (const key of Object.keys(definition.attributes)) {
     const value = definition.attributes[key];
     if (value !== undefined) {
       Base.prototype[key] = value;
     }
   }
-
   return Base;
 })();
 
@@ -32,34 +28,42 @@ const resource = (() => {
   return new (builder(Base))();
 })();
 
-async function handleJSONRPCRequest(request) {
+async function handleJSONRPCRequest(request, {awsRequestId}) {
   try {
     validateJSONRPCRequest(request);
 
-    if (
-      !(request.method === '__getMethods__' || resource.__getMethods__().includes(request.method))
-    ) {
+    let result;
+
+    if (request.method === 'invokeMethod') {
+      const {name, input, environment} = request.params || {};
+      if (!name) {
+        throw createClientError(`Method name is missing`);
+      }
+      if (definition.methods.includes(name)) {
+        const output = await resource[name](input, environment);
+        result = {output};
+      } else {
+        throw createClientError(`Remote method '${name}' doesn't exist`);
+      }
+    } else if (request.method === 'getMethods') {
+      result = definition.methods;
+    } else {
       throw createJSONRPCError(-32601);
     }
-
-    const {input, environment} = request.params || {};
-
-    const fn = resource[request.method];
-    const result = await fn.call(resource, input, environment);
 
     return buildJSONRPCResult(request.id, result);
   } catch (err) {
     let exposedError = err;
-    if (!(err.jsonRPCErrorCode || err.expose)) {
-      console.log('[ERROR] ' + err.message);
-      exposedError = new Error('An error occurred while running a resource method remotely');
+    if (!(err.jsonRPCErrorCode || isClientError(err))) {
+      console.log('[ERROR] ' + (err.stack || err.message).replace(/\n/g, ' '));
+      exposedError = new Error(`An error occurred while running a resource method remotely (awsRequestId: '${awsRequestId}')`);
     }
     return buildJSONRPCError(request.id, exposedError);
   }
 }
 
-export function handler(event, context, callback) {
-  handleJSONRPCRequest(event)
+export function handler(event, {awsRequestId}, callback) {
+  handleJSONRPCRequest(event, {awsRequestId})
     .then(result => {
       callback(null, result);
     })
