54201f7cd9c040338da593bf82feb0a076716c75
Manuel Vila <mvila@3base.com>
2018-01-26 21:39:59 +0900

    Improve Atom integration

--- integrations/atom/index.js
+++ integrations/atom/index.js
@@ -7,6 +7,8 @@ import {execFile} from 'child_process';
 import {CompositeDisposable} from 'atom';
 // import {Resource} from 'run-core';
 
+const STAGE = 'test'; // 'dev', 'test', or 'prod'
+
 export default {
   activate() {
     this.subscriptions = new CompositeDisposable();
@@ -30,6 +32,7 @@ export default {
     try {
       await this.broadcastFileModifiedEvent(event.path);
     } catch (err) {
+      console.error(err);
       const message = `resdir: An error occurred while broadcasting '@fileModified' event`;
       atom.notifications.addError(message, {detail: err.message, dismissable: true});
     }
@@ -54,9 +57,20 @@ export default {
     const directory = dirname(file);
 
     return new Promise((resolve, reject) => {
-      const command = '/Users/mvila/Projects/run/cli/dist/cjs/bin/index.js'; // /Users/mvila/Projects/run/cli/dist/cjs/bin/index.js
+      let command = 'run';
+      let env;
+      if (STAGE === 'dev') {
+        command = '/Users/mvila/Projects/run/cli/dist/cjs/bin/index.js';
+      } else if (STAGE === 'test') {
+        env = {
+          ...process.env,
+          RUN_CLIENT_DIRECTORY: '/Users/mvila/.run-test',
+          RESDIR_REGISTRY_SERVER: 'https://registry.test.resdir.com',
+          RESDIR_UPLOAD_SERVER_S3_BUCKET_NAME: 'resdir-registry-test-v1'
+        };
+      }
       const args = ['@broadcast', '--event=@fileModified', '--', `--file=${file}`, '--@quiet'];
-      const options = {cwd: directory, timeout: 60 * 1000};
+      const options = {cwd: directory, env, timeout: 60 * 1000};
       execFile(command, args, options, (err, stdout, stderr) => {
         if (stdout) {
           console.log(stdout.trim());
