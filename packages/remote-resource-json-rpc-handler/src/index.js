00e0e658e6799fbba0a5d82195479610b8c65c94
Manuel Vila <mvila@3base.com>
2018-01-28 10:33:04 +0900

    @resdir/remote-resource-json-rpc-handler: Improve protocol

--- packages/remote-resource-json-rpc-handler/src/index.js
+++ packages/remote-resource-json-rpc-handler/src/index.js
@@ -6,6 +6,8 @@ import {
 } from '@resdir/json-rpc';
 import {createClientError, isClientError} from '@resdir/error';
 
+const INVOKE_METHOD_VERSION = 1;
+
 export class RemoteResourceJSONRPCHandler {
   constructor({resource, publicMethods}) {
     this.resource = resource;
@@ -16,45 +18,55 @@ export class RemoteResourceJSONRPCHandler {
     try {
       validateJSONRPCRequest(request);
 
-      let result;
-
-      if (request.method === 'invokeMethod') {
-        const {name, input, environment} = request.params || {};
-        if (!name) {
-          throw createClientError(`Method name is missing`);
-        }
-        if (this.publicMethods.includes(name)) {
-          let output = await this.resource[name](input, environment);
-          if (output && output.$serialize) {
-            output = output.$serialize();
-          }
-          result = {output};
-        } else {
-          throw createClientError(`Remote method '${name}' doesn't exist`);
-        }
-      } else if (request.method === 'getMethods') {
-        result = this.publicMethods;
-      } else {
+      if (request.method !== 'invoke') {
         throw createJSONRPCError(-32601);
       }
 
-      return buildJSONRPCResult(request.id, result);
+      const {name, input, environment, version} = request.params || {};
+
+      if (!name) {
+        throw createClientError(`Method name is missing`);
+      }
+
+      if (version === undefined) {
+        throw createClientError(`'invoke' method version is missing`);
+      }
+
+      if (version !== INVOKE_METHOD_VERSION) {
+        throw createClientError(`'invoke' method version ${version} is unsupported`);
+      }
+
+      if (!this.publicMethods.includes(name)) {
+        throw createClientError(`Remote method '${name}' doesn't exist`);
+      }
+
+      let output = await this.resource[name](input, environment);
+
+      if (output && output.$serialize) {
+        output = output.$serialize();
+      }
+
+      return buildJSONRPCResult(request.id, {output});
     } catch (err) {
       let exposedError = err;
+
       if (!(err.jsonRPCErrorCode || isClientError(err))) {
         if (awsRequestId) {
           let message = err.stack || err.message;
-          message = message.replace(/\n/g, ' ');
+          message = message.replace(/\s+/g, ' ');
+          message = message.trim();
           console.log('[ERROR] ' + message);
         } else {
           console.error(err);
         }
+
         let exposedMessage = 'An error occurred while running a resource method remotely';
         if (awsRequestId) {
           exposedMessage += ` (awsRequestId: '${awsRequestId}')`;
         }
         exposedError = createJSONRPCError(-1, exposedMessage);
       }
+
       return buildJSONRPCError(request.id, exposedError);
     }
   }
