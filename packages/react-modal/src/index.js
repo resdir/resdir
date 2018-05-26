da18e86a549e7fea4021622b1e70a525188e6081
Manuel Vila <mvila@3base.com>
2018-05-26 16:30:49 +0900

    Make default button titles configurable

--- packages/react-modal/src/index.js
+++ packages/react-modal/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withRadiumStarter, Button} from 'radium-starter';
+import RadiumStarter, {Button} from 'radium-starter';
 import isEqual from 'lodash/isEqual';
 
 const OK_BUTTON_TITLE = 'OK';
 const CANCEL_BUTTON_TITLE = 'Cancel';
 
 export class Modal {
+  constructor({okButtonTitle = OK_BUTTON_TITLE, cancelButtonTitle = CANCEL_BUTTON_TITLE} = {}) {
+    this.okButtonTitle = okButtonTitle;
+    this.cancelButtonTitle = cancelButtonTitle;
+    this._stack = [];
+  }
+
+  getOKButtonTitle() {
+    const title = this.okButtonTitle;
+    return typeof title === 'function' ? title() : title;
+  }
+
+  getCancelButtonTitle() {
+    const title = this.cancelButtonTitle;
+    return typeof title === 'function' ? title() : title;
+  }
+
   createElement() {
     return <ModalComponent modal={this} />;
   }
-
-  _stack = [];
 
   open(attributes = {}) {
     return new Promise(resolve => {
@@ -48,7 +62,7 @@ export class Modal {
 
   async alert(message, options = {}) {
     const okButton = {
-      title: OK_BUTTON_TITLE,
+      title: this.getOKButtonTitle(),
       value: true,
       default: true
     };
@@ -64,14 +78,14 @@ export class Modal {
 
   async confirm(message, options = {}) {
     const okButton = {
-      title: OK_BUTTON_TITLE,
+      title: this.getOKButtonTitle(),
       value: true,
       default: true
     };
     Object.assign(okButton, options.okButton);
 
     const cancelButton = {
-      title: CANCEL_BUTTON_TITLE,
+      title: this.getCancelButtonTitle(),
       value: false
     };
     Object.assign(cancelButton, options.cancelButton);
@@ -100,12 +114,9 @@ export class Modal {
   }
 }
 
-@withRadiumStarter
 class ModalComponent extends React.Component {
   static propTypes = {
-    modal: PropTypes.object.isRequired,
-    theme: PropTypes.object.isRequired,
-    styles: PropTypes.object.isRequired
+    modal: PropTypes.object.isRequired
   };
 
   delayedStyle = {
@@ -123,111 +134,117 @@ class ModalComponent extends React.Component {
   }
 
   render() {
-    const {modal, theme: t, styles: s} = this.props;
-
-    const modalStyle = [
-      s.showIf(modal.getIsVisible()),
-      {
-        opacity: this.delayedStyle.opacity,
-        transition: 'opacity .1s ease-in'
-      }
-    ];
-
-    const overlayStyle = {
-      backgroundColor: t.backgroundColor,
-      opacity: 0.75,
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      bottom: 0,
-      right: 0,
-      zIndex: 20000
-    };
-
-    const dialogStyle = [
-      s.backgroundBodyColor,
-      s.border,
-      s.rounded,
-      {
-        width: '512px',
-        position: 'fixed',
-        left: '50%',
-        top: '128px',
-        marginLeft: '-256px',
-        padding: '2rem',
-        overflow: 'hidden',
-        zIndex: 20001,
-        [`@media (max-width: ${t.smallBreakpoint})`]: {
-          width: '300px',
-          marginLeft: '-150px',
-          padding: '1rem'
-        }
-      }
-    ];
-
-    const nextStyle = {
-      opacity: modal.getIsVisible() ? 1 : 0
-    };
-    if (!isEqual(nextStyle, this.delayedStyle)) {
-      this.delayedStyle = nextStyle;
-      setTimeout(() => this.forceUpdate(), 50);
-    }
-
-    const attrs = modal.getAttributes();
-    const children = [];
-
-    if (attrs.title) {
-      children.push(<h2
-        key="title"
-        style={[
-          s.regular,
-          s.secondaryTextColor,
-          s.minimumLineHeight,
-          {marginTop: '-0.25rem', marginBottom: '2rem'}
-        ]}
-      >
-        {attrs.title}
-      </h2>);
-    }
-
-    if (attrs.message) {
-      let element;
-      if (Object.prototype.hasOwnProperty.call(attrs.message, '__html')) {
-        element = <div key="message" dangerouslySetInnerHTML={attrs.message} />; // eslint-disable-line react/no-danger
-      } else {
-        element = (
-          <div key="message" style={{whiteSpace: 'pre-line'}}>
-            {attrs.message}
-          </div>
-        );
-      }
-      children.push(element);
-    }
-
-    if (attrs.buttons && attrs.buttons.length) {
-      const buttons = [];
-      for (let i = 0; i < attrs.buttons.length; i++) {
-        const button = attrs.buttons[i];
-        const props = {key: i};
-        if (button.default) {
-          props.rsAccent = true;
-        }
-        props.onClick = () => attrs._onClose(button.value);
-        if (i > 0) {
-          props.style = {marginLeft: '.5rem'};
-        }
-        buttons.push(<Button {...props}>{button.title}</Button>);
-      }
-      children.push(<div key="buttons" style={{marginTop: '2rem'}}>
-        {buttons}
-      </div>);
-    }
-
     return (
-      <div style={modalStyle}>
-        <div style={overlayStyle} />
-        <div style={dialogStyle}>{children}</div>
-      </div>
+      <RadiumStarter>
+        {(t, s) => {
+          const {modal} = this.props;
+
+          const modalStyle = [
+            s.showIf(modal.getIsVisible()),
+            {
+              opacity: this.delayedStyle.opacity,
+              transition: 'opacity .1s ease-in'
+            }
+          ];
+
+          const overlayStyle = {
+            backgroundColor: t.overlayBackgroundColor,
+            opacity: 0.66,
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            zIndex: 20000
+          };
+
+          const dialogStyle = [
+            {
+              width: '512px',
+              position: 'fixed',
+              left: '50%',
+              top: '128px',
+              marginLeft: '-256px',
+              padding: '2rem',
+              backgroundColor: t.backgroundColor,
+              overflow: 'hidden',
+              zIndex: 20001,
+              [`@media (max-width: ${t.smallBreakpoint})`]: {
+                width: '300px',
+                marginLeft: '-150px',
+                padding: '1rem'
+              }
+            },
+            s.border,
+            s.rounded
+          ];
+
+          const nextStyle = {
+            opacity: modal.getIsVisible() ? 1 : 0
+          };
+          if (!isEqual(nextStyle, this.delayedStyle)) {
+            this.delayedStyle = nextStyle;
+            setTimeout(() => this.forceUpdate(), 50);
+          }
+
+          const attrs = modal.getAttributes();
+          const children = [];
+
+          if (attrs.title) {
+            children.push(<h2
+              key="title"
+              style={[
+                s.regular,
+                s.secondaryTextColor,
+                s.minimumLineHeight,
+                {marginTop: '-0.25rem', marginBottom: '2rem'}
+              ]}
+            >
+              {attrs.title}
+            </h2>);
+          }
+
+          if (attrs.message) {
+            let element;
+            if (Object.prototype.hasOwnProperty.call(attrs.message, '__html')) {
+              element = <div key="message" dangerouslySetInnerHTML={attrs.message} />; // eslint-disable-line react/no-danger
+            } else {
+              element = (
+                <div key="message" style={{whiteSpace: 'pre-line'}}>
+                  {attrs.message}
+                </div>
+              );
+            }
+            children.push(element);
+          }
+
+          if (attrs.buttons && attrs.buttons.length) {
+            const buttons = [];
+            for (let i = 0; i < attrs.buttons.length; i++) {
+              const button = attrs.buttons[i];
+              const props = {key: i};
+              if (button.default) {
+                props.rsAccent = true;
+              }
+              props.onClick = () => attrs._onClose(button.value);
+              if (i > 0) {
+                props.style = {marginLeft: '.5rem'};
+              }
+              buttons.push(<Button {...props}>{button.title}</Button>);
+            }
+            children.push(<div key="buttons" style={{marginTop: '2rem'}}>
+              {buttons}
+            </div>);
+          }
+
+          return (
+            <div style={modalStyle}>
+              <div style={overlayStyle} />
+              <div style={dialogStyle}>{children}</div>
+            </div>
+          );
+        }}
+      </RadiumStarter>
     );
   }
 }
