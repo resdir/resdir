def50068a208d5c585a8a770d0b31acd3585a16d
Manuel Vila <mvila@3base.com>
2018-07-16 16:02:20 +0900

    aws/lambda-hosted-resource: Set Access-Control-Max-Age header for CORS

--- resources/aws/lambda-hosted-resource/src/api-gateway.js
+++ resources/aws/lambda-hosted-resource/src/api-gateway.js
@@ -122,7 +122,8 @@ export default () => ({
       responseParameters: {
         'method.response.header.Access-Control-Allow-Origin': true,
         'method.response.header.Access-Control-Allow-Headers': true,
-        'method.response.header.Access-Control-Allow-Methods': true
+        'method.response.header.Access-Control-Allow-Methods': true,
+        'method.response.header.Access-Control-Max-Age': true
       }
     });
 
@@ -144,7 +145,8 @@ export default () => ({
       responseParameters: {
         'method.response.header.Access-Control-Allow-Origin': `'*'`,
         'method.response.header.Access-Control-Allow-Headers': `'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'`,
-        'method.response.header.Access-Control-Allow-Methods': `'POST,OPTIONS'`
+        'method.response.header.Access-Control-Allow-Methods': `'POST,OPTIONS'`,
+        'method.response.header.Access-Control-Max-Age': `'300'`
       },
       responseTemplates: {
         'application/json': ''
