cc6228e9807536b207504fcab480fa4670230832
Manuel Vila <mvila@3base.com>
2018-08-28 11:07:55 +0900

    @resdir/aws-helper: Warn about AWS SDK private property use

--- packages/aws-helpers/src/iam.js
+++ packages/aws-helpers/src/iam.js
@@ -1,6 +1,7 @@
 const usernameCache = {};
 
 export async function getIAMUsername({iam}, _environment) {
+  // WARNING: Since the 'config' property is private, this might break in the future
   const {accessKeyId} = iam.client.config;
   if (!accessKeyId) {
     throw new Error(`Coudn't determine your AWS Access Key ID`);
