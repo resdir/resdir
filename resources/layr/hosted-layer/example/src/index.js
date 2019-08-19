130e6db656779d63e7199734665752c0638257d6
Manuel Vila <mvila@3base.com>
2019-08-19 17:23:27 +0900

    Start implementing layr/lambda-hosted-layer

---
+++ resources/layr/hosted-layer/example/src/index.js
@@ -0,0 +1,15 @@
+import {Model} from '@layr/model';
+import {Layer, expose} from '@layr/layer';
+
+// time curl -v -X GET http://localhost:6789
+
+// time curl -v -X POST -H "Content-Type: application/json" -d '{"query": {"Clock=>": {"getTime=>result": true}}, "source": "frontend"}' http://localhost:6789
+
+@expose()
+export class Clock extends Model {
+  @expose() static getTime() {
+    return new Date();
+  }
+}
+
+export default new Layer({Clock});
---
+++ resources/layr/hosted-layer/src/test-server.js
@@ -0,0 +1,84 @@
+import {resolve} from 'path';
+import {
+  print,
+  printSuccess,
+  formatBold,
+  formatCode,
+  formatValue,
+  formatPunctuation,
+  formatURL
+} from '@resdir/console';
+import Koa from 'koa';
+import jsonError from 'koa-json-error';
+import cors from '@koa/cors';
+import bodyParser from 'koa-bodyparser';
+import notifier from 'node-notifier';
+import sleep from 'sleep-promise';
+
+export default () => ({
+  async start({notify}, environment) {
+    const layer = this.getLayer();
+
+    const server = this.createServer(layer, environment);
+
+    server.listen(this.port, () => {
+      // TODO: Handle errors
+      printSuccess(
+        `Test server started ${formatPunctuation(`(`)}${formatURL(
+          `http://localhost:${this.port}`
+        )}${formatPunctuation(`)`)}`,
+        environment
+      );
+      if (notify) {
+        notifier.notify({title: 'Test server started', message: `http://localhost:${this.port}`});
+      }
+    });
+  },
+
+  getLayer() {
+    const parentResource = this.$getParent();
+    const main = resolve(parentResource.$getCurrentDirectory(), parentResource.main);
+    let layer = require(main);
+    if (layer.default) {
+      // ES Module
+      layer = layer.default;
+    }
+    return layer;
+  },
+
+  createServer(layer, _environment) {
+    const server = new Koa();
+
+    server.use(jsonError());
+    server.use(cors({maxAge: 900})); // 15 minutes
+    server.use(bodyParser({enableTypes: ['json'], jsonLimit: '8mb'}));
+
+    server.use(async ctx => {
+      if (this.delay) {
+        await sleep(this.delay);
+      }
+
+      if (ctx.method === 'GET') {
+        print(formatBold('→ ') + formatBold(formatCode('introspect()', {addBackticks: false})));
+        const result = layer.introspect();
+        ctx.body = result;
+        print(formatBold('← ') + formatValue(result, {multiline: false}));
+      } else if (ctx.method === 'POST') {
+        const {query, source} = ctx.request.body;
+        print(
+          formatBold('→ ') +
+            formatBold(formatCode(`invoke(`, {addBackticks: false})) +
+            formatValue({query, source}, {multiline: false}) +
+            formatBold(formatCode(`)`, {addBackticks: false}))
+        );
+        const result = await layer.receiveQuery(query, {source});
+        ctx.body = result;
+        print(formatBold('← ') + formatValue(result, {multiline: false}));
+      } else {
+        throw new Error('Invalid HTTP request');
+      }
+    });
+
+    return server;
+  }
+});
